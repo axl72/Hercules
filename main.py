b10bbe32a91a61c27ec20bad829731b8f784b07b
axl72 <axell.bernabel@unmsm.edu.pe>
2023-08-06 05:06:50 -0500

    Se automatizo la seleccion del directorio con tkinter

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import sys
 import argparse
 import os
 from controller.downloader import *
+from tkinter.filedialog import askdirectory
+from colorama import Fore, Back, Style, init
+
+# Inicialización de colorama
+init()
 
 parser = argparse.ArgumentParser(description='Process arguments')
 parser.add_argument('-u', '--url', type=str, nargs='*', required=True)
-parser.add_argument('-d', '--directory', type=str, required=False, default='./')
+parser.add_argument('-d', '--directory', type=str, required=False, default=askdirectory())
 parser.add_argument('-l', action='store_true')
 
 if __name__ == "__main__":
@@ -17,28 +22,25 @@ if __name__ == "__main__":
         title = Playlist(args.url[0]).title
         print(title)
         try:
-            path = args.directory + title
-            print("Directorio donde se descargará la playlist:  ", path)
+            path = args.directory
+            if path == "":
+                print(f"{Fore.YELLOW}Descarga cancelada{Fore.RESET}")
+                raise Exception("Ningún directorio fue seleccionado")
+
+
+            print(f"{Fore.CYAN}Directorio donde se descargará la playlist:  {path}{Fore.RESET}")
         except:
             #print(args.directory[0], type(args.directory))
             print("Directorio donde se descargará la playlist:  ", path)
             sys.exit()
-        cont = 0
-        aux_path = path
-        while(os.path.isdir(aux_path)):
-            cont += 1
-            aux_path = path + f" ({cont})"
-        path = aux_path
-        os.mkdir(path)
-
         error_list = downloadPlaylist(args.url[0], path)
+        print(f"{Fore.BLUE}Descarga terminada exitosamente{Fore.RESET}")
 
     else:
-        print("Hola")
         title = YouTube(args.url[0]).title
         try:
             path = args.directory + title
         except:
             sys.exit()
 
-        downloadVideo(args.url[0], path)
+        estate, title = downloadVideo(args.url[0], path)
