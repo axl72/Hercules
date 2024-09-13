581edff6a261a00d1f680d5ae125d0977a9fa84b
axl72 <axell.bernabel@unmsm.edu.pe>
2024-09-12 21:42:50 -0500

    MakeConfig implemented

--- config/makeconfig.py
+++ config/makeconfig.py
@@ -1,11 +1,32 @@
 import json
+import os
 
 CONFIG_PATH = "./args.conf"
 
+
 class MakeConfig:
+    config = None
 
+    @staticmethod
     def read_config():
-        file = open(CONFIG_PATH, 'r')
+        try:
+            with open(CONFIG_PATH, "r") as config_file:
+                MakeConfig.config = json.load(config_file)
+                MakeConfig.__make_download_directory__()
+                return MakeConfig.config
 
-        args = json.load(file)
-        print(args)
+        except FileNotFoundError as e:
+            print(e)
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def __make_download_directory__():
+        username = os.getlogin()
+        directory = str(MakeConfig.config["DOWNLOAD_DIRECTORY"])
+        directory = directory.replace("$USER", username)
+        MakeConfig.config["DOWNLOAD_DIRECTORY"] = directory
+
+
+if __name__ == "__main__":
+    print(MakeConfig.read_config())
